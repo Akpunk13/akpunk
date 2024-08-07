778d12a71804c7402549d29413b5bb56ca83f8e8
AfsalHashim <138692053+CyberTG@users.noreply.github.com>
2024-08-07 13:15:44 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('〄 Hᴇʟᴘ 〄', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('⌬ Gʀᴏᴜᴘ ⌬', url='t.me/MM_Archives')
+            InlineKeyboardButton('⌬ Gʀᴏᴜᴘ ⌬', url='t.me/CinemaKovilakam_Group')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
